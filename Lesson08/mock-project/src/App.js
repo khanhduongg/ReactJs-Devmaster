ba4f646fcb57aba073593ee3395fcb1e1eafef53
khanhduongg <khanhduong04.199@gmail.com>
2023-09-17 23:04:23 +0700

    Lesson08

---
+++ Lesson08/mock-project/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Title from "./components/Title";
+import ListProduct from "./components/ListProduct";
+import Cart from "./components/Cart";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        {/* TITLE : START */}
+        <Title />
+        {/* TITLE : END */}
+        <div className="row">
+          {/* LIST PRODUCT : START */}
+          <ListProduct />
+          {/* LIST PRODUCT : END */}
+          {/* CART : START */}
+        <Cart/>
+          {/* CART : END */}
+        </div>
+      </div>
+    );
+  }
+}
